3c007f87421a50b8a3fd995409bbfa5c67935c51
Zhandos Ali Brown <110089361+Zhandolia@users.noreply.github.com>
2023-12-05 13:03:42 -0500

    Add files via upload

---
+++ ChargeHotels/iOS ChargeHotels/ChargeHotels/ChargeHotels/ChargeHotelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargeHotelsApp.swift
+//  ChargeHotels
+//
+//  Created by Zhandos Brown on 05.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChargeHotelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChargeHotels/iOS ChargeHotels/main.swift
@@ -0,0 +1,11 @@
+var view = UILabel()
+
+view.frame = CGRect(x: 0, y: 0, width: 297, height: 623)
+
+view.backgroundColor = .white
+
+
+view.alpha = 0.3
+
+view.layer.backgroundColor = UIColor(red: 0.851, green: 0.851, blue: 0.851, alpha: 1).cgColor
+
